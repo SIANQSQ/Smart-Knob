c7b02bcbe8265c35a2ff7a47ab5834fc6e7fee35
SIANQSQ <qsq68686868@163.com>
2025-08-25 13:53:39 +0800

    update function

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ const char* ssid = "屈圣桥的iPhone";
 const char* pwd = "qsq060823";
 WebServer server(PORT);
 
-float KKP = 1;
+float KKP = 0.02;
 void handleSetPID()
 {
     if (server.hasArg("channel")&&server.hasArg("p")&&server.hasArg("i")&&server.hasArg("d")&&server.hasArg("limit")) 
@@ -49,20 +49,66 @@ float RAD2DEG(float RAD)
 float rd;
 float attractor = 10*PI/180.0;
 
+
+void BTN_Click()
+{
+    attractor = 1*PI/180.0;
+    Serial.println("click\n");
+    bleKeyboard.pressDial();
+    bleKeyboard.releaseDial();
+}
+
+void BTN_LongPressBegin()
+{
+    attractor = 10*PI/180.0;
+    Serial.println("LPB\n");
+    bleKeyboard.pressDial();
+}
+
+void BTN_LongPressEnd()
+{
+    Serial.println("LPE");
+    bleKeyboard.releaseDial();
+}
+
 void button_event_init() {
   button.reset();               // 清除一下按钮状态机的状态
   button.setDebounceTicks(80);  // 设置消抖时长为80毫秒,默认值为：50毫秒
   button.setClickTicks(400);    // 设置单击时长为500毫秒,默认值为：400毫秒
   button.setPressTicks(1000);   // 设置长按时长为1000毫秒,默认值为：800毫秒
 
-  button.attachClick(attachClick);                    // 初始化单击回调函数
-  button.attachLongPressStart(attachLongPressStart);  // 初始化长按开始回调函数
-  button.attachLongPressStop(attachLongPressStop);    // 初始化长按结束回调函数
+  button.attachClick(BTN_Click);                    // 初始化单击回调函数
+  button.attachLongPressStart(BTN_LongPressBegin);  // 初始化长按开始回调函数
+  button.attachLongPressStop(BTN_LongPressEnd);    // 初始化长按结束回调函数
+}
+
+float preAngle=0;
+void Knob()
+{
+    //BLDC.SetVelocityAngel(serial_motor_target());
+    rd = round(BLDC.GetAngel()/attractor)*attractor;
+    BLDC.SetTorque(KKP*(rd - BLDC.GetAngel()));
+
+    if(BLDC.GetAngel()-preAngle>DEG2RAD(30)) 
+    {
+        Serial.print("rotate");
+        bleKeyboard.rotate(1);
+        preAngle = BLDC.GetAngel();
+    }
+    else if (BLDC.GetAngel()-preAngle<-DEG2RAD(30))
+    {
+        Serial.print("rotate");
+        bleKeyboard.rotate(-1);
+        preAngle = BLDC.GetAngel();
+    }
+    
 }
 
 void loop() 
 {     
-  //设置PID
+    button.tick(); // 让OneButton库处理按钮状态
+    Knob();
+    //设置PID
 //   DFOC_M0_SET_ANGLE_PID(1.8,0,0,0);
 //   DFOC_M0_SET_VEL_PID(0.01,0.00,0,0);
 //   //给库设定速度
@@ -70,9 +116,7 @@ void loop()
 //   //接收串口
     //serialReceiveUserCommand();
     //server.handleClient();
-    //BLDC.SetVelocityAngel(serial_motor_target());
-    // rd = round(BLDC.GetAngel()/attractor)*attractor;
-    // BLDC.SetTorque(KKP*(rd - BLDC.GetAngel()));
+    // 
     
     // bleKeyboard.pressDial();
     // delay(1);
@@ -80,11 +124,11 @@ void loop()
     // delay(1);
     // bleKeyboard.releaseDial();
     // delay(1);
-    bleKeyboard.pressDial();
-    Serial.println("press");
-    delay(1000);
-    bleKeyboard.releaseDial();
-    bleKeyboard.rotate(1);
+    // bleKeyboard.pressDial();
+    // Serial.println("press");
+    // delay(1000);
+    // bleKeyboard.releaseDial();
+    // bleKeyboard.rotate(1);
     delay(100);
 
 
@@ -102,11 +146,10 @@ void setup() {
   Serial.print("Ssssssss");
   BLDC.SetPID_Angel(0.06,0.0004,0.0003,100,0);
   BLDC.SetPID_Velocity(1.2,0.00,0,6.3,0);
-  //BLDC1.SetPID_Angel(0.06,0.0004,0.0003,100,0);
-  //BLDC1.SetPID_Velocity(1.2,0.00,0,6.3,0);
 //   server.on("/",handleRoot);
 //   server.on("/setpid",handleSetPID);
 //   server.begin();
 //   Serial.println("HTTP server started");
+  button_event_init();
   bleKeyboard.begin();
 }
